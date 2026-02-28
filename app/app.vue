8f3954595d9c63885309035f7e97cc346b5943b9
Saurlax <saurlax@qq.com>
2026-03-01 02:24:58 +0800

    feat: init nuxt ui

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <UButton>Click me</UButton>
+  </UApp>
 </template>
+
+<style>
+@import "tailwindcss";
+@import "@nuxt/ui";
+
+@theme {
+  --color-primary: #11a8cd;
+}
+</style>
